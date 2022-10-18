2f6b0d9957438db5f422ce9c4f95f2177f33d832
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2022-10-18 16:00:35 +0300

    Add Polish and Italian language

---
+++ src/locale/it.php
@@ -0,0 +1,29 @@
+<?php 
+
+$lang = array(
+
+    'days' => array(        
+        'Sunday'    => 'Domenica',
+        'Monday'    => 'Lunedì',
+        'Tuesday'   => 'Martedì',
+        'Wednesday' => 'Mercoledì',
+        'Thursday'  => 'Giovedì',
+        'Friday'    => 'Venerdì',
+        'Saturday'  => 'Sabato',        
+    ),
+    
+    'months' => array(
+        'January'   => 'Gennaio',
+        'February'  => 'Febbraio',
+        'March'     => 'Marzo',
+        'April'     => 'Aprile',
+        'May'       => 'Maggio',
+        'June'      => 'Giugno',
+        'July'      => 'Luglio',
+        'August'    => 'Agosto',
+        'September' => 'Settembre',
+        'October'   => 'Ottobre',
+        'November'  => 'Novembre',
+        'December'  => 'Dicembre',
+    )
+);
---
+++ src/locale/pl.php
@@ -0,0 +1,29 @@
+<?php 
+
+$lang = array(
+
+    'days' => array(        
+        'Sunday'    => 'Niedziela',
+        'Monday'    => 'Poniedziałek',
+        'Tuesday'   => 'Wtorek',
+        'Wednesday' => 'Środa',
+        'Thursday'  => 'Czwartek',
+        'Friday'    => 'Piątek',
+        'Saturday'  => 'Sobota',        
+    ),
+    
+    'months' => array(
+        'January'   => 'Styczeń',
+        'February'  => 'Luty',
+        'March'     => 'Marzec',
+        'April'     => 'Kwiecień',
+        'May'       => 'Maj',
+        'June'      => 'Czerwiec',
+        'July'      => 'Lipiec',
+        'August'    => 'Sierpień',
+        'September' => 'Wrzesień',
+        'October'   => 'Październik',
+        'November'  => 'Listopad',
+        'December'  => 'Grudzień',
+    )
+);
